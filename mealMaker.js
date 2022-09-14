358b150281fc358557a40caebe12e6c826622d05
DEric33 <eric.darmusie@darmuweb.com>
2022-09-14 16:56:36 +0200

    code identique mais resultat different le debut commencerait avec _menu à la place de _meal mais ca ne conduit pas a un meilleur resultat

--- mealMaker.js
+++ mealMaker.js
@@ -1,1 +1,30 @@
-console.log('Coucou');
+const menu = {
+    _menu: '',
+    _price: 0,
+    
+    set meal(mealToCheck){
+        if (typeof mealToCheck === 'string'){
+          return this._meal=mealToCheck;
+        }
+    },
+    set meal(priceToCheck){
+        if (typeof priceToCheck === 'number'){
+            return this._price=priceToCheck;
+        }
+    },
+    get todaysSpecial(){
+        if (this._meal && this._price) {
+            return `Today's Special is ${this._meal} for $${this._price}!`
+        }else{
+            return `Meal or price was not set correctly!`
+        }
+    }
+  }
+  menu.meal='chocolate';
+  menu.price=12;
+
+ // console.log(menu)
+  // console.log(meal,' : ',price);
+ console.log(menu.todaysSpecial);
+
+ // console.log(typeof meal);
